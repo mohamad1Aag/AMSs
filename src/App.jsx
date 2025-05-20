5d33dab4500d0b6f4b55ed150091612601ab39e1
Mohammad <aagmohamad4@gmail.com>
2025-05-20 12:36:01 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Services from "./components/Services"; // مثال صفحة خدمات
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Business from "./components/Business";
 
-import './App.css'
-import Home from "./components/Home.jsx"
 function App() {
-  
   return (
-    
-    
-        <Home />
-        
-        
-    
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/business" element={<Business />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,7 @@
 // src/components/Home.jsx
 import React, { useState } from "react";
+import { Link } from "react-router-dom";  // استيراد Link
 import './Home.css';
-
-
 
 function Home() {
   const [navVisible, setNavVisible] = useState(false);
@@ -16,25 +15,28 @@ function Home() {
       <header className="header">
         <div className="logo"></div>
         <button className="menu-btn" onClick={toggleNav} >
-        <svg width="30" height="30" viewBox="0 0 100 80" fill="#00b4db">
-    <rect width="100" height="15"></rect>
-    <rect y="30" width="100" height="15"></rect>
-    <rect y="60" width="100" height="15"></rect>
-  </svg> {/* أيقونة الهامبرغر */}
+          <svg width="30" height="30" viewBox="0 0 100 80" fill="#00b4db">
+            <rect width="100" height="15"></rect>
+            <rect y="30" width="100" height="15"></rect>
+            <rect y="60" width="100" height="15"></rect>
+          </svg> {/* أيقونة الهامبرغر */}
         </button>
         <nav className={`nav ${navVisible ? "active" : ""}`}>
-          <a href="#">logo</a>
-          <a href="#">Home</a>
-          <a href="/services">Services</a>          <a href="#">AboutUs</a>
-          <a href="#">Contact</a>
-          <a href="#">Our Business</a>
+          <Link to="/">Logo</Link>
+          <Link to="/">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/about">AboutUs</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/business">Our Business</Link>
         </nav>
       </header>
 
       <div className="content">
         <h3>اهلا بكم في موقع AMS.WEB لتطوير وبرمجة مواقع الويب</h3>
         <div className="main-box">AMS.WEB</div>
-        <button className="btn">الذهاب الي صفحة الخدمات</button>
+        <Link to="/services">
+          <button className="btn">الذهاب الي صفحة الخدمات</button>
+        </Link>
       </div>
     </div>
   )
